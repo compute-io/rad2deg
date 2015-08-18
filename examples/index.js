a894470f4244224af77053f34e80c2f3c6ad8ec8
Planeshifter <pburckhardt@outlook.com>
2015-08-18 23:09:06 +0200

    [UPDATE] README.md, examples

--- examples/index.js
+++ examples/index.js
@@ -1,15 +1,85 @@
 'use strict';
 
-var rad2deg = require( './../lib' );
+var matrix = require( 'dstructs-matrix' ),
+	rad2deg = require( './../lib' );
 
-// Simulate some data...
-var data = new Array( 100 );
+var data,
+	mat,
+	out,
+	tmp,
+	i;
 
-var twopi = 2*Math.PI;
-for ( var i = 0; i < data.length; i++ ) {
-	data[ i ] = Math.random()*twopi;
+var TWO_PI = 2 * Math.PI;
+
+// ----
+// Plain arrays...
+data = new Array( 10 );
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = Math.random() * TWO_PI;
 }
+out = rad2deg( data );
+console.log( 'Arrays: %s\n', out );
 
-rad2deg( data );
 
-console.log( data.join( '\n' ) );
+// ----
+// Object arrays (accessors)...
+function getValue( d ) {
+	return d.x;
+}
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = {
+		'x': data[ i ]
+	};
+}
+out = rad2deg( data, {
+	'accessor': getValue
+});
+console.log( 'Accessors: %s\n', out );
+
+
+// ----
+// Deep set arrays...
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = {
+		'x': [ i, data[ i ].x ]
+	};
+}
+out = rad2deg( data, {
+	'path': 'x/1',
+	'sep': '/'
+});
+console.log( 'Deepset:');
+console.dir( out );
+console.log( '\n' );
+
+
+// ----
+// Typed arrays...
+data = new Int32Array( 10 );
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = Math.random() * TWO_PI;
+}
+tmp = rad2deg( data );
+out = '';
+for ( i = 0; i < data.length; i++ ) {
+	out += tmp[ i ];
+	if ( i < data.length-1 ) {
+		out += ',';
+	}
+}
+console.log( 'Typed arrays: %s\n', out );
+
+
+// ----
+// Matrices...
+mat = matrix( data, [5,2], 'int32' );
+out = rad2deg( mat );
+console.log( 'Matrix: %s\n', out.toString() );
+
+
+// ----
+// Matrices (custom output data type)...
+out = rad2deg( mat, {
+	'dtype': 'uint8'
+});
+console.log( 'Matrix (%s): %s\n', out.dtype, out.toString() );
